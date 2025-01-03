0fa0eb45292048bc227e45da8b2b76c0d09c0365
ReactiveX22 <akibreactive@gmail.com>
2025-01-03 13:06:52 +0600

    fix: adjust header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ import { CodeIcon } from '../icons/Icons';
 
 export const Header = () => {
   return (
-    <div className='fixed top-0 z-10 h-[55px] w-full text-nowrap bg-bg-900 text-lg shadow-md'>
-      <div className='mx-auto flex h-full w-full max-w-[1152px] justify-between px-2'>
+    <div className='fixed top-0 z-10 w-full text-nowrap bg-bg-900 text-lg shadow-md'>
+      <div className='mx-auto flex h-full w-full max-w-[1152px] items-center justify-between px-2 py-1'>
         <Link
           to='/'
           className='flex h-full items-center justify-center gap-2 py-2 text-2xl'
