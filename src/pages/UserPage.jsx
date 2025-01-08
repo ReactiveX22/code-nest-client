44ded98e764f4bf1a2cbd070cdc26de500a5be95
ReactiveX22 <akibreactive@gmail.com>
2025-01-08 13:40:22 +0600

    feat: add loading for userPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -7,19 +7,32 @@ import SkeletonPosts from '../components/skeletons/SkeletonPosts';
 export const UserPage = () => {
   const { user } = useLoaderData();
   const [userPosts, setUserPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [timeoutReached, setTimeoutReached] = useState(false);
 
   useEffect(() => {
     const fetchUserPosts = async () => {
       try {
         const posts = await getPostsByUserId(user.id);
         setUserPosts(posts);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching user posts:', error);
+        setLoading(false);
       }
     };
 
     fetchUserPosts();
-  }, [user]);
+
+    const timeout = setTimeout(() => {
+      if (userPosts.length === 0) {
+        setTimeoutReached(true);
+        setLoading(false);
+      }
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, [user, userPosts.length]);
 
   return (
     <div className='flex flex-col gap-4'>
@@ -27,7 +40,12 @@ export const UserPage = () => {
         Recent Posts by {user.username}
       </div>
       <div className='mb-3 pb-4'>
-        {userPosts.length === 0 && <SkeletonPosts />}
+        {loading && !timeoutReached && <SkeletonPosts />}
+        {timeoutReached && userPosts.length === 0 && (
+          <div className='w-full self-center text-center text-xl font-semibold text-neutral'>
+            No posts found
+          </div>
+        )}
         {userPosts.length > 0 && <PostCardList posts={userPosts} />}
       </div>
     </div>
